ed5c9fe842a3fc6be4b83f06c855cf0c9ee857f6
Ardhiyan Abdul Hakim <ardhiyan.hakim.bsn@gmail.com>
2022-03-29 03:23:22 +0700

    Putting Notes Plugin into Server Hapi

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 /* eslint-disable no-unused-vars */
 
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
 
-const init = async (request, response) => {
+const init = async () => {
+  const notesService = new NotesService();
+
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
@@ -14,10 +17,15 @@ const init = async (request, response) => {
     },
   });
 
-  server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
 
   await server.start();
-  console.log(`Server saat ini berjalan di ${server.info.uri}`);
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
