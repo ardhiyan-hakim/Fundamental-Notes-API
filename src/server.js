d6d3439d09cc576e5f8732090b34d5dee9dbde12
Ardhiyan Abdul Hakim <ardhiyan.hakim.bsn@gmail.com>
2022-04-15 14:13:13 +0700

    Insert Users Plugin onto Server

--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
-const notes = require('./api/notes');
 
+const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -25,6 +30,13 @@ const init = async () => {
       options: {
         service: notesService,
         validator: NotesValidator,
+      },
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
       },
     },
   ]);
